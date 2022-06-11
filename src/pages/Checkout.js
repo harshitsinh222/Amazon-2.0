83f4101de14a9971fc456e9dbf511c563f33e6e3
Harshitsinh Vala <80336177+harshitsinh222@users.noreply.github.com>
2022-06-10 23:38:53 -0400

    Finalev

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -8,7 +8,7 @@ import Currency from "react-currency-formatter";
 import { useSession } from "next-auth/client";
 import { loadStripe } from "@stripe/stripe-js";
 import axios from "axios";
-const stripePromise = loadStripe(process.env.stripe_public_key);
+const stripePromise = loadStripe(`${process.env.stripe_public_key}`);
 
 function Checkout() {
   const items = useSelector(selectItems);
